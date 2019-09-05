109eed5f8e3d5e8b7a45356d93ec49805920e5ca
mafub <mattbrooker22@gmail.com>
2019-09-05 20:08:07 +0100

    User screen two

---
+++ app/src/main/java/com/example/minimoneybox/UserAccountsActivity.kt
@@ -0,0 +1,14 @@
+package com.example.minimoneybox
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class UserAccountsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+
+    
+}
